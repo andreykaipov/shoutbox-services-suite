f81a2b349cba161d04dc2daa0b134d28cfb57e97
Andrey Kaipov <akaip002@fiu.edu>
2017-06-14 21:34:48 -0400

    add logging

---
+++ utils/logger.ts
@@ -0,0 +1,31 @@
+import * as winston from 'winston'
+
+export default function Logger(prefix: string, logFile: boolean | string = true) {
+
+  const logFormat = options => `\
+    [${prefix}] ${options.level.toUpperCase()} ${options.timestamp()} ${options.message ? options.message : ''}\
+    ${options.meta && Object.keys(options.meta).length ? `\nmeta: ${JSON.stringify(options.meta)}` : ''}\
+  `.trim()
+  
+  const transports: any = [
+    new winston.transports.Console({
+      timestamp: () => new Date().toISOString(),
+      formatter: logFormat
+    })
+  ]
+
+  if (logFile) {
+    logFile = typeof logFile === 'string' ? logFile : `${prefix.toLowerCase()}.log`
+    transports.push(
+      new winston.transports.File({
+        filename: `./logs/${logFile}`
+      })
+    )
+  }
+  
+  return new winston.Logger({
+    transports: transports
+  })
+  
+}
+
