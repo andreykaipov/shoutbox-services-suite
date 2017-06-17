18be48d03b13b55f29c9f18fe1602ab1c85ad117
Andrey Kaipov <akaip002@fiu.edu>
2017-06-17 18:20:28 -0400

    add repository service

---
+++ services/repository/app.ts
@@ -0,0 +1,27 @@
+import config from '../../utils/config'
+const logger = config.Logger('SHOUTS_REPOSITORY_APP')
+
+import * as amqp from 'amqplib'
+import { Rabbit } from '../../utils/rabbit'
+import { ProcessedShout } from '../processor/shouts-processor'
+import { initMongo } from './shouts-repository'
+
+logger.info('Started repository service...')
+
+~async function startPersisting() {
+
+  const channel = await Rabbit.getChannel()
+  const mongo = await initMongo()
+
+  const consumer = channel.consume(config.RABBIT.REPOSITORY.INBOUND_QUEUE, msg => {
+
+    const payload = msg.content.toString()
+    const processedShout = JSON.parse(payload) as ProcessedShout
+    mongo.persistShout(processedShout)
+    channel.ack(msg)
+
+  }, { noAck: false })
+
+  consumer.catch(async () => await Rabbit.close())
+
+}()
---
+++ services/repository/shouts-repository.ts
@@ -0,0 +1,70 @@
+import config from '../../utils/config'
+const logger = config.Logger('SHOUTS_REPOSITORY')
+
+import * as mongojs from 'mongojs'
+import { ProcessedShout } from '../processor/shouts-processor'
+
+let shouts
+let users
+
+export async function initMongo() {
+  const db = mongojs(process.env.SSS_MONGO_CS)
+  shouts = db.collection('shouts')
+  users = db.collection('users')
+  return {
+    persistShout: persistShout
+  }
+}
+
+async function persistShout(shout: ProcessedShout) {
+
+  shouts.insert({
+    _id: shout.id,
+    timestamp: shout.timestamp,
+    author_id: shout.authorId,
+    author_name: shout.authorName,
+    author_color: shout.authorColor,
+    content: shout.content
+  })
+
+  trackUser(shout)
+
+}
+
+/* Tracks a user by the given shout.
+ * Only thing we can practically track is a user's past names! */
+async function trackUser(shout: ProcessedShout) {
+
+  const userId = shout.authorId
+  const userName = shout.authorName
+  const timestamp = shout.timestamp
+
+  users.find({ _id: userId }, (err, docs) => {
+
+    if (docs.length === 0) {
+      users.insert({
+        _id: userId,
+        current_name: userName,
+        name_first_seen: timestamp,
+        past_names: []
+      })
+    } else {
+      const userDocument = docs.pop() // there's only ever gonna be one
+      if (userDocument.current_name === userName) {
+        return false
+      }
+      userDocument.past_names.push({
+        name: userDocument.current_name,
+        name_first_seen: userDocument.name_first_seen
+      })
+      this.users.update({ _id: userId }, {
+        _id: userId,
+        current_name: userName,
+        name_first_seen: timestamp,
+        past_names: userDocument.past_names
+      })
+    }
+
+  })
+
+}
